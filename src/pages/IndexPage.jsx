86a7fc928eefaf69fc83f786fcf25f59a5741ca0
Hugo Moreira <hugo.n4@gmail.com>
2023-02-11 09:26:34 -0800

    boilerplate with navbar

---
+++ src/pages/IndexPage.jsx
@@ -0,0 +1,17 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
+
+export default function IndexPage() {
+  const [places,setPlaces] = useState([]);
+  useEffect(() => {
+    axios.get('/places').then(response => {
+      setPlaces(response.data);
+    });
+  }, []);
+  return (
+    <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
+      Index Page
+    </div>
+  );
+}
